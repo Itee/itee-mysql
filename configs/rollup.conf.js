45b1fbcdd4823186a2fc2603c02d249c3582f818
Tristan VALCKE <valcketristan@gmail.com>
2021-07-07 23:50:21 +0200

    build(rollup): upgrade rollup plugin to monorepo version

--- configs/rollup.conf.js
+++ configs/rollup.conf.js
@@ -15,11 +15,11 @@
 
 /* eslint-env node */
 
-const packageInfos = require( '../package' )
-const commonjs     = require( 'rollup-plugin-commonjs' )
-const path         = require( 'path' )
-const resolve      = require( 'rollup-plugin-node-resolve' )
-const terser       = require( 'rollup-plugin-terser' ).terser
+const packageInfos    = require( '../package' )
+const path            = require( 'path' )
+const commonjs        = require( '@rollup/plugin-commonjs' )
+const { nodeResolve } = require( '@rollup/plugin-node-resolve' )
+const terser          = require( 'rollup-plugin-terser' ).terser
 
 function _computeBanner ( name, format ) {
 
@@ -93,7 +93,7 @@ function CreateRollupConfigs ( options ) {
                     commonjs( {
                         include: 'node_modules/**'
                     } ),
-                    resolve( {
+                    nodeResolve( {
                         preferBuiltins: true
                     } ),
                     isProd && terser()
